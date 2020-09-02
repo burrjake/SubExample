dd4513e0b126ff4463ff02a0c6b6648184e008d8
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-02 08:57:21 -0600

    Add project files.

---
+++ SubExample.vb
@@ -0,0 +1,16 @@
+﻿Module SubExample
+
+    Sub Main()
+        'this code runs at runn up
+        Console.WriteLine("In Sub Main")
+        Console.ReadLine()
+    End Sub
+
+    Sub SubExample()
+        'This code only runs when it is "called"
+        Console.WriteLine("In Sub SubExample")
+        Console.ReadLine()
+    End Sub
+
+    Sub SubWithArguments()
+End Module
